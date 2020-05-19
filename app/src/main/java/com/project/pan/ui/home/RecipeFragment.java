ce557d8bc45ce7608b61906ee1c16995b52b8d8b
nanayaarashi <nanaya.arashi@gmail.com>
2020-05-20 00:58:22 +0800

    [Hotpan] - new recycle view for recipe text

--- app/src/main/java/com/project/pan/ui/home/RecipeFragment.java
+++ app/src/main/java/com/project/pan/ui/home/RecipeFragment.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.project.pan.R;
 
@@ -19,6 +21,8 @@ import java.util.List;
 
 public class RecipeFragment extends Fragment {
     private List<String> mRecipeSteps;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,22 +34,33 @@ public class RecipeFragment extends Fragment {
         TextView mRecipeQuantity = root.findViewById(R.id.recipe_quantity);
         TextView mRecipeText = root.findViewById(R.id.recipe_text);
 
-        getSteps();
         Log.d("===Recipe Fragment===", "get recipe bundle");
+        getSteps();
         mRecipeImg.setImageResource(getArguments().getInt("recipe_img"));
         mRecipeTitle.setText(getArguments().getString("recipe_title"));
         mRecipeMaterial.setText(getArguments().getString("recipe_material"));
         mRecipeQuantity.setText(getArguments().getString("recipe_quantity"));
-        if(mRecipeSteps.size() > 0){
 
+        RecyclerView mRecipeStep = root.findViewById(R.id.recipe_recycle_text);
+        mRecipeStep.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this.getContext());
+        mRecipeStep.setLayoutManager(layoutManager);
+        mAdapter = new RecipeStepAdapter(this.getContext(), mRecipeSteps);
+
+        if(mRecipeSteps != null){
+            mRecipeStep.setVisibility(View.VISIBLE);
+            mRecipeText.setVisibility(View.GONE);
+            mRecipeStep.setAdapter(mAdapter);
         } else {
+            mRecipeStep.setVisibility(View.GONE);
+            mRecipeText.setVisibility(View.VISIBLE);
             mRecipeText.setText(getArguments().getString("recipe_text"));
         }
 
         return root;
     }
 
-    public void getSteps(){
+    private void getSteps(){
         String rawText = getArguments().getString("recipe_text");
         if("".equals(rawText)){
             return;
--- app/src/main/java/com/project/pan/ui/home/RecipeStepAdapter.java
+++ app/src/main/java/com/project/pan/ui/home/RecipeStepAdapter.java
@@ -1,6 +1,7 @@
 package com.project.pan.ui.home;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,14 +27,14 @@ public class RecipeStepAdapter extends RecyclerView.Adapter<RecipeStepAdapter.Vi
     public RecipeStepAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(mContext).inflate(R.layout.recipe_text_line, parent, false);
         ViewHolder holder = new ViewHolder(view);
-        holder.stepNumber = (TextView) view.findViewById(R.id.recipe_step);
+        holder.stepNumber = (TextView) view.findViewById(R.id.recipe_step_number);
         holder.stepDetail = (TextView) view.findViewById(R.id.recipe_step_text);
-        return null;
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecipeStepAdapter.ViewHolder holder, int position) {
-        String number = String.valueOf(position+1)+". ";
+        String number = (position + 1) +". ";
         String oneStep = mRecipeSteps.get(position);
         holder.stepNumber.setText(number);
         holder.stepDetail.setText(oneStep);
@@ -44,9 +45,9 @@ public class RecipeStepAdapter extends RecyclerView.Adapter<RecipeStepAdapter.Vi
         return mRecipeSteps.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView stepNumber;
-        public TextView stepDetail;
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView stepNumber;
+        TextView stepDetail;
 
         public ViewHolder(View itemView) {
             super(itemView);

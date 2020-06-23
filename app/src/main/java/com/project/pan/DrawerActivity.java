ffbc948846e45a173efc84a58f53f1ae43bbe921
nanayaarashi <nanaya.arashi@gmail.com>
2020-06-23 18:10:00 +0800

    [Hotpan] -Add bluetooth control.

--- app/src/main/java/com/project/pan/DrawerActivity.java
+++ app/src/main/java/com/project/pan/DrawerActivity.java
@@ -1,10 +1,14 @@
 package com.project.pan;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHeadset;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -31,14 +36,20 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 import com.project.pan.R;
+import com.project.pan.backstage.PanController;
 import com.project.pan.ui.settings.SettingsFragment;
 import com.project.pan.ui.viewpager.RecipeSaver;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.UUID;
 
 import static androidx.navigation.ui.NavigationUI.setupWithNavController;
 
@@ -46,6 +57,7 @@ public class DrawerActivity extends AppCompatActivity implements View.OnClickLis
     // Intent request codes
     private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
     private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+
 
     private AppBarConfiguration mAppBarConfiguration;
     private DrawerLayout drawer;
@@ -58,11 +70,12 @@ public class DrawerActivity extends AppCompatActivity implements View.OnClickLis
      * Local Bluetooth adapter
      */
     private BluetoothAdapter mBluetoothAdapter = null;
+    public BluetoothSocket mBluetoothSocket = null;
+    private BluetoothDevice mBluetoothDevice = null;
+    private String mAddress = null;
+    // SPP UUID service - this should work for most devices
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
-    /**
-     * Member object for the chat services
-     */
-    private BluetoothChatService mChatService = null;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -80,8 +93,9 @@ public class DrawerActivity extends AppCompatActivity implements View.OnClickLis
         try{
             mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
             mBluetoothAdapter.enable();
+            mAddress = this.getIntent().getExtras().getString(DevicesListActivity.EXTRA_DEVICE_ADDRESS);
             Log.d("====addr====", this.getIntent().getExtras().getString(DevicesListActivity.EXTRA_DEVICE_ADDRESS));
-            connectDevice(this.getIntent(), true);
+            bluetoothConnect(mAddress);
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -110,42 +124,76 @@ public class DrawerActivity extends AppCompatActivity implements View.OnClickLis
         setButton.setOnClickListener(this);
 
     }
-    /**
-     * Establish connection with other device
-     *
-     * @param data   An {@link Intent} with {@link DeviceListActivity#EXTRA_DEVICE_ADDRESS} extra.
-     * @param secure Socket Security type - Secure (true) , Insecure (false)
-     */
-    private void connectDevice(Intent data, boolean secure) {
-        // Get the device MAC address
-        Bundle extras = data.getExtras();
-        if (extras == null) {
-            return;
+
+    public synchronized void bluetoothDisconnect() {
+        try {
+            if(mBluetoothSocket != null) {
+                //Don't leave Bluetooth sockets open when leaving activity
+                mBluetoothSocket.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            //insert code to deal with this
         }
-        String address = extras.getString(DevicesListActivity.EXTRA_DEVICE_ADDRESS);
-        Log.d("====addr====", address);
-        // Get the BluetoothDevice object
-        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
-        // Attempt to connect to the device
-        mChatService.connect(device, secure);
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case REQUEST_CONNECT_DEVICE_SECURE:
-                // When DeviceListActivity returns with a device to connect
-                if (resultCode == Activity.RESULT_OK) {
-                    connectDevice(data, true);
-                }
-                break;
-            case REQUEST_CONNECT_DEVICE_INSECURE:
-                // When DeviceListActivity returns with a device to connect
-                if (resultCode == Activity.RESULT_OK) {
-                    connectDevice(data, false);
-                }
-                break;
+    public synchronized void bluetoothConnect(String addr) {
+        if(!checkBTState()) {
+            return;
         }
+        boolean condition;
+        condition = addr != null;
+        condition &= (mBluetoothAdapter != null) && (mBluetoothAdapter.isEnabled());
+        condition &= (mBluetoothAdapter != null) && (mBluetoothAdapter.getProfileConnectionState(BluetoothHeadset.HEADSET) != BluetoothHeadset.STATE_CONNECTED);
+
+        if(condition) {
+            //create device and set the MAC address
+            mBluetoothDevice = mBluetoothAdapter.getRemoteDevice(addr);
+
+            try {
+                mBluetoothSocket = createBluetoothSocket(mBluetoothDevice);
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_LONG).show();
+            }
+            // Establish the Bluetooth socket connection.
+            try {
+                mBluetoothSocket.connect();
+            } catch (IOException e) {
+                try {
+                    mBluetoothSocket.close();
+                } catch (IOException e2) {
+                    //insert code to deal with this
+                }
+            }
+        }
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+
+        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        //creates secure outgoing connecetion with BT device using UUID
+    }
+
+    //Checks that the Android device Bluetooth is available and prompts to be turned on if off
+    private boolean checkBTState() {
+
+        if(mBluetoothAdapter == null) {
+            Toast.makeText(getBaseContext(), "Device does not support bluetooth", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                mBluetoothAdapter.enable();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+        return true;
     }
 
     @Override
@@ -202,7 +250,14 @@ public class DrawerActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onResume() {
         super.onResume();
-        EventBus.getDefault().register(this);
+        bluetoothConnect(mAddress);
+        //EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        bluetoothDisconnect();
     }
 
     @Override
